a6b43948c6c16b989622b20e36fe895baf470278
hmpears2 <hmpears2@uncg.edu>
2025-10-05 18:09:36 -0400

    Added initial scaffolding files

---
+++ src/test/java/com/practice/RestfulCRUDAPIPractice/RestfulCrudapiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.RestfulCRUDAPIPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulCrudapiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
